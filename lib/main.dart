d34fb8c539b64c0e1786b8b968aebea7ef2d1502
bhuvana2817 <bhuvanajangam8@gmail.com>
2023-08-06 16:50:50 +0530

    First

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:dreamfarm/Signin.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  //hlafkjh
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DearFarm',
+      debugShowCheckedModeBanner: false,
+      home: const SignInpageWidget(),
+    );
+  }
+}
